24345cc6c55d0fc206a59e81106603fa18cc3ddd
Yurills <rawityurill@gmail.com>
2024-03-25 00:53:51 +0700

    fix some error, make buttommenu stay at the footer, add comment date

--- src/components/ReviewBlock.tsx
+++ src/components/ReviewBlock.tsx
@@ -1,11 +1,13 @@
 import { Rating } from "@mui/material"
 
-export default function ReviewBlock( {user,rating, comment} : {user:string, rating:number, comment: string}) {
+export default function ReviewBlock( {user,rating, comment, createdAt} : {user:string, rating:number, comment: string, createdAt: string}) {
     return (
-        <div className="mx-auto mt-1 mb-5 p-5 h-[auto] w-[95%] border border-solid border-slate-500 rounded-2xl">
+        <div className="mx-auto mt-1 mb-5 p-5 pb-2 h-[auto] w-[95%] border border-solid border-slate-500 rounded-2xl">
                 <p className="font-semibold">{user} </p>
                 <Rating value={rating} readOnly></Rating>
-                <p>{comment}</p>
+                <p className="pb-3">{comment}</p>
+
+                <p className="text-orange-500 text-xs ">this comment is created at: {createdAt}</p>
 
         </div>
     )
