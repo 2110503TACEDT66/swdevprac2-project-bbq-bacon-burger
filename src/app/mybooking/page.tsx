a5b95e9e1e80817965382271e12a4a19d5527251
Thamvarut Wannachetisara <rebeerreo@gmail.com>
2024-03-25 20:21:11 +0700

    getbookings

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,26 +1,33 @@
-//temporaly mybooking page
-'use client'
 
 import Image from "next/image"
 import CartItem from "../../components/CartItem"
-export default function Cart({ hotelName, imgSrc }: { hotelName: string, imgSrc: string }) {
-    // const session = true
-    // return (
-    //     <div>
-    //         <div className="flex justify-center items-center text-3xl pt-10">
-    //             My Booking
-    //             <Image src="/img/hotel-logo.png" alt="hotel-logo" width={40} height={40} className="ml-2" />
-    //         </div>
+import getBookings from "@/libs/getBookings"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route";
+export default async function mybooking() {
+    const session = await getServerSession(authOptions);
 
-    //          {  session == true ?
-    //             <div className="flex flex-row pl-20 pr-20 pt-10 h-auto bg-paper justify-center items-center">
-    //                 <div className="w-[80%] h-full">
-    //                     <CartItem hotelName="Reo Hotel 200Bth" imgSrc="/img/banner1.png" price={200} buttonName="Cancle"/>
-    //                     <CartItem hotelName="Reo Hotel 300Bitcoin" imgSrc="/img/banner1.png" price={2300} buttonName="Cancle"/>
-    //                 </div>
+    if (session) {
+        const bookings = await getBookings(session.user.token);
+        // Rest of the code using 'bookings'
+        console.log(bookings);
+    }
+    return (
+        <div>
+            <div className="flex justify-center items-center text-3xl pt-10">
+                My Booking
+                <Image src="/img/hotel-logo.png" alt="hotel-logo" width={40} height={40} className="ml-2" />
+            </div>
+
+             {  session ?
+                <div className="flex flex-row pl-20 pr-20 pt-10 h-auto bg-paper justify-center items-center">
+                    <div className="w-[80%] h-full">
+                        
+                        
+                    </div>
                     
-    //             </div> : <div className="flex justify-center h-auto bg-paper m-10">No Booking D:</div>
-    //         }
-    //     </div>
-    // )
+                </div> : <div className="flex justify-center h-auto bg-paper m-10">No Booking D:</div>
+            }
+        </div>
+    )
 }
