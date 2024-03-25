50ef5e4057f2b82bd3614c14ee15167a4f39c757
DESKTOP-AI9JUBD\user <p51104.2015@gmail.com>
2024-03-26 00:07:15 +0700

    add search bar

--- src/components/HotelCatalog.tsx
+++ src/components/HotelCatalog.tsx
@@ -1,20 +1,57 @@
+'use client'
+
 import Link from "next/link";
 import HotelCard from "./HotelCard";
+import { useState, useEffect } from "react";
+import getHotels from "@/libs/getHotels";
 
-export default async function HotelCatalog({hotelJson}:{hotelJson:Object}){
-    const gethotelJson = await hotelJson
-    return(
-        <>
-        {/* <div>{gethotelJson.count}</div> */}
+export default function HotelCatalog({ hotelJson }: { hotelJson: any }) {
+    const [hotelData, setHotelData] = useState<any>();
+    const [search, setSearch] = useState('');
+
+    useEffect( ()=> {
+        const fetchUserData = async () => {
+            try {
+                const result = await getHotels()
+                setHotelData(result);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchUserData();
+    },[])
+
+    console.log(search);
+    console.log(hotelData);
+
+    return (
+        <div className="justify-center item-center">
             <div className="container mx-auto my-8 p-4 rounded-lg shadow-md block">
-                {
-                    gethotelJson.data.map((hotelItem:Object)=>(
-                        <Link key={hotelItem.name} href={`/hotel/${hotelItem.id}`}>
-                            <HotelCard hotelName={hotelItem.name} imgSrc={`/img/${hotelItem.file}`} hotelAddress={hotelItem.address} hotelTel={hotelItem.tel} hotelPrice={hotelItem.price}/>
-                        </Link>
-                    ))   
-                }
+                <div className="flex flex-row ml-[20%]">
+                    <input
+                        type="text"
+                        id="search"
+                        name="search"
+                        placeholder="Search..."
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="input input-bordered w-[70%] text-l p-2 mr-[2%]"
+                    />
+                </div>
+                {hotelData?.data.filter((hotelItem)=>{
+                    return search.toLowerCase() === ''? hotelItem: hotelItem.name.toLowerCase().includes(search)
+                }).map((hotelItem: any) => (
+                    <Link key={hotelItem.name} href={`/hotel/${hotelItem.id}`}>
+                        <HotelCard
+                            hotelName={hotelItem.name}
+                            imgSrc={`/img/${hotelItem.file}`}
+                            hotelAddress={hotelItem.address}
+                            hotelTel={hotelItem.tel}
+                            hotelPrice={hotelItem.price}
+                        />
+                    </Link>
+                ))}
             </div>
-        </>
-    )
+        </div>
+    );
 }
