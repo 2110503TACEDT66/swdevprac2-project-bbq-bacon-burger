2e5eec35b924c32511c7570c6050c016c379cc2c
Thamvarut Wannachetisara <rebeerreo@gmail.com>
2024-03-26 17:54:11 +0700

    remove setTimeOut in getHotels

--- src/libs/getHotels.tsx
+++ src/libs/getHotels.tsx
@@ -1,7 +1,4 @@
-
 export default async function getHotels(){
-    await new Promise((resolve)=> setTimeout(resolve,1000))
-
     const response = await fetch('https://presentation-day-1-bbq-bacon-burger.vercel.app/api/v1/hotels/',{mode:'cors'})
     if(!response.ok){
         throw new Error("Fail to Fetch Hotel")
