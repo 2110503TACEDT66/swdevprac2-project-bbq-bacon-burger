ba162bd12af9763916fcf859aab35f8b129b6332
Thamvarut Wannachetisara <rebeerreo@gmail.com>
2024-03-26 21:42:53 +0700

    temporary change OTP to localhost

--- src/libs/sentOTP.tsx
+++ src/libs/sentOTP.tsx
@@ -1,5 +1,5 @@
 export default async function sentOTP(userEmail: string) {
-    const response = await fetch("https://presentation-day-1-bbq-bacon-burger.vercel.app/api/v1/otp/sent-otp", {
+    const response = await fetch("http://localhost:5000/api/v1/otp/sent-otp", {
         method: "POST",
         mode:"cors",
         headers: {
